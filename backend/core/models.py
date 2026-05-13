3a6d4e48cfed98efb588a44685d6529e3567ecfe
Anyika Michelle <m.anyika@alustudent.com>
2026-05-13 10:53:34 +0200

    add bestpractices on tier

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,15 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+tier_free= 'Free'
+tier_premium = 'Premium'
+tier_admin = 'Admin'
 class User(AbstractUser):
+
     TIER_CHOICES = (
-        ('Free', 'Free'),
-        ('Premium', 'Premium'),
-        ('Admin', 'Admin'),
+        (tier_free, tier_free),
+        (tier_premium, tier_premium),
+        (tier_admin, tier_admin),
     )
     email = models.EmailField(unique=True, blank=False, null=False)
     is_premium = models.BooleanField(default=False)
-    tier = models.CharField(max_length=20, choices=TIER_CHOICES, default='Free')
+    tier = models.CharField(max_length=20, choices=TIER_CHOICES, default=tier_free)
 
     class Meta:
         verbose_name = "User"
